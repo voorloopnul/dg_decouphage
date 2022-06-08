92b2e2ca12aefa4d4d75d8b5d1358c50a8147f8d
Ricardo Pascal <voorloop@gmail.com>
2022-06-08 22:48:33 +0200

    -

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,10 @@ import base64
 import os
 import sys
 
+import requests as requests
+
 url = base64.b64decode(sys.argv[1]).decode("utf-8")
 os.system(f'wget "{url}" -O input_file')
 os.system(f"python3 decouphage.py input_file")
+with open('output.gbk', 'rb') as data:
+    requests.put(url, data=data)
