b7e792005da75f7f082c21be458d243949f9f2de
Ricardo Pascal <voorloop@gmail.com>
2022-06-15 23:46:57 +0200

    -

--- pipeline.py
+++ pipeline.py
@@ -2,28 +2,32 @@ import base64
 import os
 import sys
 from subprocess import run
-
 import requests as requests
 
 url = base64.b64decode(sys.argv[1]).decode("utf-8")
-cmd = " ".join(sys.argv[2:])
+parameters = " ".join(sys.argv[2:])
+
+# Download input file
+#
 
 ret = os.system(f'wget "{url}" -O input_file')
-print(ret)
 
-decouphage = run(f"python3 decouphage.py --tmp_dir output --output output/output.gbk {cmd} input_file")
-print(decouphage.returncode)
+# Run decouphage
+#
 
-ret = os.system('pwd')
-print(ret)
+cmd = f"python3 decouphage.py --tmp_dir output --output output/output.gbk {parameters} input_file"
+decouphage = run(cmd.split(" "))
+if decouphage.returncode:
+    sys.exit(1)
 
-ret = os.system("ls")
-print(ret)
+# Compress output
+#
 
-ret = os.system("tar -zcvf output.tar.gz output")
-print(ret)
+os.system("tar -zcvf output.tar.gz output")
+
+# Upload output
+#
 
 with open('output.tar.gz', 'rb') as data:
     r = requests.put(os.getenv("DOCKGRID_RESULT_URL"), data=data)
-    print(r.status_code)
     print(r.status_code)
